16e7f98f1f6147e521b9c25dc65ac9f2862e51bd
TakWolf <takwolf@foxmail.com>
2023-05-23 14:39:08 +0800

    加载字形上下文

---
+++ common/services/design_service.py
@@ -0,0 +1,52 @@
+import logging
+import os
+
+from common.services.font_service import FontConfig
+from common.utils import glyph_util
+
+logger = logging.getLogger('design-service')
+
+
+class DesignContext:
+    def __init__(
+            self,
+            alphabet: list[str],
+            not_def_glyph_file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes],
+            code_point_to_glyph_file_path: dict[int, str | bytes | os.PathLike[str] | os.PathLike[bytes]],
+    ):
+        self.alphabet = alphabet
+        self.not_def_glyph_file_path = not_def_glyph_file_path
+        self.code_point_to_glyph_file_path = code_point_to_glyph_file_path
+
+
+def format_glyphs(font_config: FontConfig):
+    for glyph_file_dir, _, glyph_file_names in os.walk(font_config.glyphs_dir):
+        for glyph_file_name in glyph_file_names:
+            if not glyph_file_name.endswith('.png'):
+                continue
+            glyph_file_path = os.path.join(glyph_file_dir, glyph_file_name)
+            glyph_data = glyph_util.load_data_from_png(glyph_file_path)[0]
+            glyph_util.save_data_to_png(glyph_data, glyph_file_path)
+            logger.info(f'format glyph file: {glyph_file_path}')
+
+
+def load_context(font_config: FontConfig) -> DesignContext:
+    alphabet = set()
+    not_def_glyph_file_path = None
+    code_point_to_glyph_file_path = {}
+    for glyph_file_dir, _, glyph_file_names in os.walk(font_config.glyphs_dir):
+        for glyph_file_name in glyph_file_names:
+            if not glyph_file_name.endswith('.png'):
+                continue
+            glyph_file_path = os.path.join(glyph_file_dir, glyph_file_name)
+            if glyph_file_name == 'notdef.png':
+                not_def_glyph_file_path = glyph_file_path
+            else:
+                uni_hex_name = glyph_file_name.removesuffix('.png').upper()
+                code_point = int(uni_hex_name, 16)
+                c = chr(code_point)
+                alphabet.add(c)
+                code_point_to_glyph_file_path[code_point] = glyph_file_path
+    alphabet = list(alphabet)
+    alphabet.sort()
+    return DesignContext(alphabet, not_def_glyph_file_path, code_point_to_glyph_file_path)
--- examples/demo.py
+++ examples/demo.py
@@ -1,9 +1,15 @@
 from common import path_define
+from common.services import design_service
 from common.services.font_service import FontConfig
+from common.utils import fs_util
 
 
 def main():
+    fs_util.delete_dir(path_define.build_dir)
+
     font_config = FontConfig(path_define.glyphs_dir)
+    design_service.format_glyphs(font_config)
+    context = design_service.load_context(font_config)
 
 
 if __name__ == '__main__':
