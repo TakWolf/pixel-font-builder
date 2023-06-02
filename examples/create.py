2ad7d2af141e72b865f7d8fc9e40280b1d54e741
TakWolf <takwolf@foxmail.com>
2023-06-03 01:18:02 +0800

    重构 Demo

--- examples/create.py
+++ examples/create.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
-    font_builder = FontBuilder(
+    builder = FontBuilder(
         size=12,
         ascent=10,
         descent=-2,
@@ -16,11 +16,11 @@ def main():
         cap_height=7,
     )
 
-    font_builder.character_mapping = {
+    builder.character_mapping = {
         ord('A'): 'CAP_LETTER_A',
     }
 
-    font_builder.add_glyph(Glyph(
+    builder.add_glyph(Glyph(
         name='.notdef',
         advance_width=8,
         offset=(0, -2),
@@ -39,7 +39,7 @@ def main():
             [1, 1, 1, 1, 1, 1, 1, 1],
         ],
     ))
-    font_builder.add_glyph(Glyph(
+    builder.add_glyph(Glyph(
         name='CAP_LETTER_A',
         advance_width=8,
         offset=(0, -2),
@@ -59,24 +59,24 @@ def main():
         ],
     ))
 
-    font_builder.meta_infos.version = '1.0.0'
-    font_builder.meta_infos.family_name = 'Demo Pixel'
-    font_builder.meta_infos.style_name = StyleName.REGULAR
-    font_builder.meta_infos.serif_mode = SerifMode.SANS_SERIF
-    font_builder.meta_infos.width_mode = WidthMode.MONOSPACED
-    font_builder.meta_infos.manufacturer = 'TakWolf Studio'
-    font_builder.meta_infos.designer = 'TakWolf'
-    font_builder.meta_infos.description = 'A demo pixel font.'
-    font_builder.meta_infos.copyright_info = 'Copyright (c) TakWolf'
-    font_builder.meta_infos.license_info = 'This Font Software is licensed under the SIL Open Font License, Version 1.1.'
-    font_builder.meta_infos.vendor_url = 'https://github.com/TakWolf/pixel-font-builder'
-    font_builder.meta_infos.designer_url = 'https://takwolf.com'
-    font_builder.meta_infos.license_url = 'https://scripts.sil.org/OFL'
+    builder.meta_infos.version = '1.0.0'
+    builder.meta_infos.family_name = 'Demo Pixel'
+    builder.meta_infos.style_name = StyleName.REGULAR
+    builder.meta_infos.serif_mode = SerifMode.SANS_SERIF
+    builder.meta_infos.width_mode = WidthMode.MONOSPACED
+    builder.meta_infos.manufacturer = 'TakWolf Studio'
+    builder.meta_infos.designer = 'TakWolf'
+    builder.meta_infos.description = 'A demo pixel font.'
+    builder.meta_infos.copyright_info = 'Copyright (c) TakWolf'
+    builder.meta_infos.license_info = 'This Font Software is licensed under the SIL Open Font License, Version 1.1.'
+    builder.meta_infos.vendor_url = 'https://github.com/TakWolf/pixel-font-builder'
+    builder.meta_infos.designer_url = 'https://takwolf.com'
+    builder.meta_infos.license_url = 'https://scripts.sil.org/OFL'
 
-    font_builder.save_otf(os.path.join(outputs_dir, 'demo.otf'))
-    font_builder.save_otf(os.path.join(outputs_dir, 'demo.woff2'), flavor=opentype.Flavor.WOFF2)
-    font_builder.save_ttf(os.path.join(outputs_dir, 'demo.ttf'))
-    font_builder.save_bdf(os.path.join(outputs_dir, 'demo.bdf'))
+    builder.save_otf(os.path.join(outputs_dir, 'demo.otf'))
+    builder.save_otf(os.path.join(outputs_dir, 'demo.woff2'), flavor=opentype.Flavor.WOFF2)
+    builder.save_ttf(os.path.join(outputs_dir, 'demo.ttf'))
+    builder.save_bdf(os.path.join(outputs_dir, 'demo.bdf'))
 
 
 if __name__ == '__main__':
--- examples/demo.py
+++ examples/demo.py
@@ -1,23 +1,123 @@
 import logging
 import os
+import tomllib
 
 from examples import glyphs_dir, outputs_dir
-from examples.services import font_service, design_service
-from examples.services.font_service import FontConfig
-from pixel_font_builder import opentype
+from examples.utils import glyph_util
+from pixel_font_builder import FontBuilder, Glyph, StyleName, SerifMode, WidthMode, opentype
 
 logging.basicConfig(level=logging.DEBUG)
 
 
+class FontConfig:
+    def __init__(self, glyphs_dir: str):
+        self.glyphs_dir = glyphs_dir
+
+        config_file_path = os.path.join(glyphs_dir, 'config.toml')
+        with open(config_file_path, 'rb') as file:
+            config_data = tomllib.load(file)['font']
+
+        self.size: int = config_data['size']
+        self.line_height: int = config_data['line_height']
+        self.box_origin_y: int = config_data['box_origin_y']
+        self.ascent: int = round((self.line_height - self.size) / 2 + self.box_origin_y)
+        self.descent: int = self.ascent - self.line_height
+        self.x_height: int = config_data['x_height']
+        self.cap_height: int = config_data['cap_height']
+
+
+def _get_glyph_name(code_point: int) -> str:
+    return f'uni{code_point:04X}'
+
+
+def _format_glyphs(font_config: FontConfig):
+    for glyph_file_dir, _, glyph_file_names in os.walk(font_config.glyphs_dir):
+        for glyph_file_name in glyph_file_names:
+            if not glyph_file_name.endswith('.png'):
+                continue
+            glyph_file_path = os.path.join(glyph_file_dir, glyph_file_name)
+            glyph_data = glyph_util.load_data_from_png(glyph_file_path)[0]
+            glyph_util.save_data_to_png(glyph_data, glyph_file_path)
+
+
+def _collect_glyphs(font_config: FontConfig) -> tuple[dict[int, str], dict[str, str]]:
+    character_mapping = {}
+    glyph_file_paths = {}
+    for glyph_file_dir, _, glyph_file_names in os.walk(font_config.glyphs_dir):
+        for glyph_file_name in glyph_file_names:
+            if not glyph_file_name.endswith('.png'):
+                continue
+            glyph_file_path = os.path.join(glyph_file_dir, glyph_file_name)
+            if glyph_file_name == 'notdef.png':
+                glyph_file_paths['.notdef'] = glyph_file_path
+            else:
+                code_point = int(glyph_file_name.removesuffix('.png'), 16)
+                glyph_name = _get_glyph_name(code_point)
+                character_mapping[code_point] = glyph_name
+                glyph_file_paths[glyph_name] = glyph_file_path
+    return character_mapping, glyph_file_paths
+
+
+def _create_builder(
+        font_config: FontConfig,
+        character_mapping: dict[int, str],
+        glyph_file_paths: dict[str, str],
+) -> FontBuilder:
+    builder = FontBuilder(
+        font_config.size,
+        font_config.ascent,
+        font_config.descent,
+        font_config.x_height,
+        font_config.cap_height,
+    )
+
+    builder.character_mapping.update(character_mapping)
+    for code_point in range(ord('A'), ord('Z') + 1):
+        fallback_code_point = code_point + ord('Ａ') - ord('A')
+        builder.character_mapping[fallback_code_point] = _get_glyph_name(code_point)
+    for code_point in range(ord('a'), ord('z') + 1):
+        fallback_code_point = code_point + ord('ａ') - ord('a')
+        builder.character_mapping[fallback_code_point] = _get_glyph_name(code_point)
+    for code_point in range(ord('0'), ord('9') + 1):
+        fallback_code_point = code_point + ord('０') - ord('0')
+        builder.character_mapping[fallback_code_point] = _get_glyph_name(code_point)
+
+    for glyph_name, glyph_file_path in glyph_file_paths.items():
+        glyph_data, glyph_width, glyph_height = glyph_util.load_data_from_png(glyph_file_path)
+        offset_y = round((glyph_height - font_config.size) / 2 + font_config.box_origin_y) - glyph_height
+        builder.add_glyph(Glyph(
+            name=glyph_name,
+            advance_width=glyph_width,
+            offset=(0, offset_y),
+            data=glyph_data,
+        ))
+
+    builder.meta_infos.version = '1.0.0'
+    builder.meta_infos.family_name = 'Cute Pixel'
+    builder.meta_infos.style_name = StyleName.REGULAR
+    builder.meta_infos.serif_mode = SerifMode.SANS_SERIF
+    builder.meta_infos.width_mode = WidthMode.PROPORTIONAL
+    builder.meta_infos.manufacturer = 'TakWolf Studio'
+    builder.meta_infos.designer = 'TakWolf'
+    builder.meta_infos.description = 'A demo pixel font.'
+    builder.meta_infos.copyright_info = 'Copyright (c) TakWolf'
+    builder.meta_infos.license_info = 'This Font Software is licensed under the SIL Open Font License, Version 1.1.'
+    builder.meta_infos.vendor_url = 'https://github.com/TakWolf/pixel-font-builder'
+    builder.meta_infos.designer_url = 'https://takwolf.com'
+    builder.meta_infos.license_url = 'https://scripts.sil.org/OFL'
+
+    return builder
+
+
 def main():
     font_config = FontConfig(glyphs_dir)
-    design_service.format_glyphs(font_config)
-    design_context = design_service.load_context(font_config)
-    font_builder = font_service.create_font_builder(font_config, design_context)
-    font_builder.save_otf(os.path.join(outputs_dir, 'cute.otf'))
-    font_builder.save_otf(os.path.join(outputs_dir, 'cute.woff2'), flavor=opentype.Flavor.WOFF2)
-    font_builder.save_ttf(os.path.join(outputs_dir, 'cute.ttf'))
-    font_builder.save_bdf(os.path.join(outputs_dir, 'cute.bdf'))
+    _format_glyphs(font_config)
+    character_mapping, glyph_file_paths = _collect_glyphs(font_config)
+    builder = _create_builder(font_config, character_mapping, glyph_file_paths)
+    builder.save_otf(os.path.join(outputs_dir, 'cute.otf'))
+    builder.save_otf(os.path.join(outputs_dir, 'cute.woff2'), flavor=opentype.Flavor.WOFF2)
+    builder.save_ttf(os.path.join(outputs_dir, 'cute.ttf'))
+    builder.save_bdf(os.path.join(outputs_dir, 'cute.bdf'))
 
 
 if __name__ == '__main__':
