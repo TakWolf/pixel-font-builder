6c6588f0e144eb4fdd2548777950c00f7f4150db
TakWolf <takwolf@foxmail.com>
2023-08-05 04:03:20 +0800

    调整 Demo，使用配置文件加载字体信息

--- examples/demo.py
+++ examples/demo.py
@@ -6,7 +6,7 @@ import tomllib
 import png
 
 from examples import glyphs_dir, outputs_dir
-from pixel_font_builder import FontBuilder, FontCollectionBuilder, Glyph, StyleName, SerifMode, WidthMode, opentype
+from pixel_font_builder import FontBuilder, FontCollectionBuilder, Glyph, opentype
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -20,14 +20,25 @@ class FontConfig:
             config_data: dict = tomllib.load(file)['font']
 
         self.size: int = config_data['size']
-        self.line_height: int = config_data['line_height']
-        assert (self.line_height - self.size) % 2 == 0
-
-        self.box_origin_y: int = config_data['box_origin_y']
-        self.ascent = self.box_origin_y + (self.line_height - self.size) // 2
-        self.descent = self.ascent - self.line_height
+        self.ascent: int = config_data['ascent']
+        self.descent: int = config_data['descent']
         self.x_height: int = config_data['x_height']
         self.cap_height: int = config_data['cap_height']
+
+        self.version: str = config_data['version']
+        self.family_name: str = config_data['family_name']
+        self.style_name: str = config_data['style_name']
+        self.serif_mode: str = config_data['serif_mode']
+        self.width_mode: str = config_data['width_mode']
+        self.manufacturer: str = config_data['manufacturer']
+        self.designer: str = config_data['designer']
+        self.description: str = config_data['description']
+        self.copyright_info: str = config_data['copyright_info']
+        self.license_info: str = config_data['license_info']
+        self.vendor_url: str = config_data['vendor_url']
+        self.designer_url: str = config_data['designer_url']
+        self.license_url: str = config_data['license_url']
+        self.sample_text: str = config_data['sample_text']
 
 
 def _load_glyph_data_from_png(file_path: str) -> tuple[list[list[int]], int, int]:
@@ -92,9 +103,9 @@ def _collect_glyph_files(font_config: FontConfig) -> tuple[dict[int, str], dict[
 def _create_builder(
         font_config: FontConfig,
         glyph_cacher: dict[str, Glyph],
-        family_name: str,
         character_mapping: dict[int, str],
         glyph_file_paths: dict[str, str],
+        name_num: int = None,
 ) -> FontBuilder:
     builder = FontBuilder(
         font_config.size,
@@ -120,7 +131,7 @@ def _create_builder(
             glyph = glyph_cacher[glyph_file_path]
         else:
             glyph_data, glyph_width, glyph_height = _load_glyph_data_from_png(glyph_file_path)
-            offset_y = font_config.box_origin_y + math.ceil((glyph_height - font_config.size) / 2) - glyph_height
+            offset_y = math.floor((font_config.ascent + font_config.descent - glyph_height) / 2)
             glyph = Glyph(
                 name=glyph_name,
                 advance_width=glyph_width,
@@ -130,20 +141,22 @@ def _create_builder(
             glyph_cacher[glyph_file_path] = glyph
         builder.add_glyph(glyph)
 
-    builder.meta_infos.version = '1.0.0'
-    builder.meta_infos.family_name = family_name
-    builder.meta_infos.style_name = StyleName.REGULAR
-    builder.meta_infos.serif_mode = SerifMode.SANS_SERIF
-    builder.meta_infos.width_mode = WidthMode.PROPORTIONAL
-    builder.meta_infos.manufacturer = 'Pixel Font Studio'
-    builder.meta_infos.designer = 'TakWolf'
-    builder.meta_infos.description = 'A demo pixel font.'
-    builder.meta_infos.copyright_info = 'Copyright (c) TakWolf'
-    builder.meta_infos.license_info = 'This Font Software is licensed under the SIL Open Font License, Version 1.1.'
-    builder.meta_infos.vendor_url = 'https://github.com/TakWolf/pixel-font-builder'
-    builder.meta_infos.designer_url = 'https://takwolf.com'
-    builder.meta_infos.license_url = 'https://scripts.sil.org/OFL'
-    builder.meta_infos.sample_text = 'Hello World!'
+    builder.meta_infos.version = font_config.version
+    builder.meta_infos.family_name = font_config.family_name
+    if name_num is not None:
+        builder.meta_infos.family_name += f' {name_num}'
+    builder.meta_infos.style_name = font_config.style_name
+    builder.meta_infos.serif_mode = font_config.serif_mode
+    builder.meta_infos.width_mode = font_config.width_mode
+    builder.meta_infos.manufacturer = font_config.manufacturer
+    builder.meta_infos.designer = font_config.designer
+    builder.meta_infos.description = font_config.description
+    builder.meta_infos.copyright_info = font_config.copyright_info
+    builder.meta_infos.license_info = font_config.license_info
+    builder.meta_infos.vendor_url = font_config.vendor_url
+    builder.meta_infos.designer_url = font_config.designer_url
+    builder.meta_infos.license_url = font_config.license_url
+    builder.meta_infos.sample_text = font_config.sample_text
 
     return builder
 
@@ -154,16 +167,16 @@ def main():
     character_mapping, glyph_file_paths = _collect_glyph_files(font_config)
     glyph_cacher = {}
 
-    builder = _create_builder(font_config, glyph_cacher, 'Demo Pixel', character_mapping, glyph_file_paths)
+    builder = _create_builder(font_config, glyph_cacher, character_mapping, glyph_file_paths)
     builder.save_otf(os.path.join(outputs_dir, 'demo.otf'))
     builder.save_otf(os.path.join(outputs_dir, 'demo.woff2'), flavor=opentype.Flavor.WOFF2)
     builder.save_ttf(os.path.join(outputs_dir, 'demo.ttf'))
     builder.save_bdf(os.path.join(outputs_dir, 'demo.bdf'))
 
     collection_builder = FontCollectionBuilder()
-    for index in range(1000):
-        builder = _create_builder(font_config, glyph_cacher, f'Demo Pixel {index}', character_mapping, glyph_file_paths)
-        builder.opentype_configs.cff_family_name = 'Demo Pixel'
+    for index in range(100):
+        builder = _create_builder(font_config, glyph_cacher, character_mapping, glyph_file_paths, index)
+        builder.opentype_configs.cff_family_name = font_config.family_name
         collection_builder.font_builders.append(builder)
     collection_builder.save_otc(os.path.join(outputs_dir, 'demo.otc'))
     collection_builder.save_ttc(os.path.join(outputs_dir, 'demo.ttc'))
