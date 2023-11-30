68c3ccad5db50c45c6fe6a7560a09b4414476e3c
TakWolf <takwolf@foxmail.com>
2023-11-30 14:35:56 +0800

    调整路径配置

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,6 +1,5 @@
 import os
 
 project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-assets_dir = os.path.join(project_root_dir, 'assets')
-glyphs_dir = os.path.join(assets_dir, 'glyphs')
+glyphs_dir = os.path.join(project_root_dir, 'assets', 'glyphs')
 build_dir = os.path.join(project_root_dir, 'build')
