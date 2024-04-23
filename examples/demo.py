e24c679b03fdec32695278e506cfcec335024d60
TakWolf <takwolf@foxmail.com>
2024-04-24 01:59:46 +0800

    代码整理

--- examples/demo.py
+++ examples/demo.py
@@ -10,12 +10,12 @@ from pixel_font_builder import FontBuilder, FontCollectionBuilder, StyleName, Se
 
 
 def _load_glyph_data_from_png(file_path: str) -> tuple[list[list[int]], int, int]:
-    width, height, bitmap, _ = png.Reader(filename=file_path).read()
+    width, height, pixels, _ = png.Reader(filename=file_path).read()
     data = []
-    for bitmap_row in bitmap:
+    for pixels_row in pixels:
         data_row = []
         for x in range(0, width * 4, 4):
-            alpha = bitmap_row[x + 3]
+            alpha = pixels_row[x + 3]
             if alpha > 127:
                 data_row.append(1)
             else:
@@ -25,19 +25,19 @@ def _load_glyph_data_from_png(file_path: str) -> tuple[list[list[int]], int, int
 
 
 def _save_glyph_data_to_png(data: list[list[int]], file_path: str):
-    bitmap = []
+    pixels = []
     for data_row in data:
-        bitmap_row = []
+        pixels_row = []
         for x in data_row:
-            bitmap_row.append(0)
-            bitmap_row.append(0)
-            bitmap_row.append(0)
+            pixels_row.append(0)
+            pixels_row.append(0)
+            pixels_row.append(0)
             if x == 0:
-                bitmap_row.append(0)
+                pixels_row.append(0)
             else:
-                bitmap_row.append(255)
-        bitmap.append(bitmap_row)
-    png.from_array(bitmap, 'RGBA').save(file_path)
+                pixels_row.append(255)
+        pixels.append(pixels_row)
+    png.from_array(pixels, 'RGBA').save(file_path)
 
 
 class GlyphFile:
