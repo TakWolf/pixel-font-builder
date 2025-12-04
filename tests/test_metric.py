e86dc3c92eb48edc94d85129334ee323db4b2b58
TakWolf <takwolf@foxmail.com>
2025-12-04 14:14:06 +0800

    代码整理

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -2,6 +2,25 @@ from pixel_font_builder import LineMetric, FontMetric
 
 
 def test_font_metric_1():
+    font_metric = FontMetric()
+    assert font_metric.font_size == 0
+    assert font_metric.horizontal_layout.ascent == 0
+    assert font_metric.horizontal_layout.descent == 0
+    assert font_metric.horizontal_layout.line_height == 0
+    assert font_metric.horizontal_layout.line_gap == 0
+    assert font_metric.vertical_layout.ascent == 0
+    assert font_metric.vertical_layout.descent == 0
+    assert font_metric.vertical_layout.line_height == 0
+    assert font_metric.vertical_layout.line_gap == 0
+    assert font_metric.x_height == 0
+    assert font_metric.cap_height == 0
+    assert font_metric.underline_position == 0
+    assert font_metric.underline_thickness == 0
+    assert font_metric.strikeout_position == 0
+    assert font_metric.strikeout_thickness == 0
+
+
+def test_font_metric_2():
     font_metric = FontMetric(
         font_size=10,
         horizontal_layout=LineMetric(
@@ -36,22 +55,3 @@ def test_font_metric_1():
     assert font_metric.underline_thickness == 16
     assert font_metric.strikeout_position == 18
     assert font_metric.strikeout_thickness == 20
-
-
-def test_font_metric_2():
-    font_metric = FontMetric()
-    assert font_metric.font_size == 0
-    assert font_metric.horizontal_layout.ascent == 0
-    assert font_metric.horizontal_layout.descent == 0
-    assert font_metric.horizontal_layout.line_height == 0
-    assert font_metric.horizontal_layout.line_gap == 0
-    assert font_metric.vertical_layout.ascent == 0
-    assert font_metric.vertical_layout.descent == 0
-    assert font_metric.vertical_layout.line_height == 0
-    assert font_metric.vertical_layout.line_gap == 0
-    assert font_metric.x_height == 0
-    assert font_metric.cap_height == 0
-    assert font_metric.underline_position == 0
-    assert font_metric.underline_thickness == 0
-    assert font_metric.strikeout_position == 0
-    assert font_metric.strikeout_thickness == 0
