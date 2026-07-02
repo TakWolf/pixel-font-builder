a71e0957299e2cdaa27637ccacc57f28a1c0bf0d
TakWolf <takwolf@foxmail.com>
2026-07-02 16:12:11 +0800

    [OpenType] table__g_l_y_f_zero_length.compile() 明确返回 0 字节

--- src/pixel_font_builder/opentype/patch/_g_l_y_f.py
+++ src/pixel_font_builder/opentype/patch/_g_l_y_f.py
@@ -5,9 +5,8 @@ from fontTools.ttLib.tables import DefaultTable
 class table__g_l_y_f_zero_length(DefaultTable.DefaultTable):
     def __init__(self):
         super().__init__('glyf')
-        self.data = b''
 
     def compile(self, ttFont: TTFont) -> bytes:
         if 'loca' in ttFont:
             ttFont['loca'].set([0])
-        return self.data
+        return b''
